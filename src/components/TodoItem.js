8f655a6c5fbe677cfe4d1089b39c208491b40b9d
ppby <salswnss@gmail.com>
2020-07-26 15:53:10 +0900

    Add TodoItem component

---
+++ src/components/TodoItem.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { CheckOutlined, DeleteOutlined } from '@ant-design/icons';
+import styled, { css } from 'styled-components';
+
+// 우측에 나타나는 쓰레기통 보여주는 컴포넌트
+const Remove = styled.div`
+  opacity: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+`;
+// 왼쪽에 있는 체크를 보여주는 컴포넌트
+const CheckCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  border: 1px solid #ced4da;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+  ${(props) =>
+    props.done &&
+    css`
+      font-size: 18px; /*font-size가 의미하는 건 아이콘 크기*/
+      border: 1px solid #38d9a0;
+      color: #38d9a9;
+    `}
+`;
+// 텍스트를 보여주는 컴포넌트
+const Text = styled.div`
+  flex: 1;
+  font-size: 21px;
+  color: #495057;
+  ${(props) =>
+    props.done &&
+    css`
+      color: #ced4da;
+    `}
+`;
+// 위 3개의 컴포넌트가 안으로 들어가지게 만드는 컴포넌트
+const TodoItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  &:hover {
+    ${Remove} {
+      opacity: 1;
+    }
+  }
+`;
+
+const TodoItem = ({ id, done, text }) => {
+  return (
+    <div>
+      <TodoItemBlock>
+        <CheckCircle done={done}>{done && <CheckOutlined />}</CheckCircle>
+        <Text done={done}>{text}</Text>
+        <Remove>
+          <DeleteOutlined />
+        </Remove>
+      </TodoItemBlock>
+    </div>
+  );
+};
+
+export default TodoItem;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import TodoItem from './TodoItem';
 
 const TodoListBlock = styled.div`
   flex: 1; /* 자신이 차지할 수 있는 영역 다 차지함 (display: flex가 된 상황에서) */
@@ -9,7 +10,14 @@ const TodoListBlock = styled.div`
 `;
 
 const TodoList = () => {
-  return <TodoListBlock>TodoListItem</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItem text="프로젝트 생성하기" done={true} />
+      <TodoItem text="컴포넌트 스타일링 하기" done={false} />
+      <TodoItem text="아랑" done={false} />
+      <TodoItem text="알ㄹ아라" done={true} />
+    </TodoListBlock>
+  );
 };
 
 export default TodoList;
