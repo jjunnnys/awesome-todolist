6fd961cfa73624e7d8aa248d96cc8dc33350fa9a
ppby <salswnss@gmail.com>
2020-07-26 17:03:18 +0900

    Modify TodoLIst state

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -11,13 +11,17 @@ const TodoListBlock = styled.div`
 `;
 
 const TodoList = () => {
-  const state = useTodoState();
+  const todos = useTodoState();
   return (
     <TodoListBlock>
-      <TodoItem text="프로젝트 생성하기" done={true} />
-      <TodoItem text="컴포넌트 스타일링 하기" done={false} />
-      <TodoItem text="아랑" done={false} />
-      <TodoItem text="알ㄹ아라" done={true} />
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}
+        />
+      ))}
     </TodoListBlock>
   );
 };
